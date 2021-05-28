e19b0cfcb0beedfe2f62246c2db78aeeb3a85d95
fblanchetNaN <florian.blanchet.supop@gmail.com>
2021-05-28 15:05:26 +0300

    fix: tests.utils.mock_stdin for list and empty

--- tests/utils.py
+++ tests/utils.py
@@ -4,6 +4,6 @@ from incipyt._internal.utils import is_nonstring_sequence
 
 
 def mock_stdin(monkeypatch, inputs):
-    if is_nonstring_sequence(inputs):
+    if not is_nonstring_sequence(inputs):
         inputs = (inputs,)
-    monkeypatch.setattr("sys.stdin", io.StringIO("\n".join(inputs)))
+    monkeypatch.setattr("sys.stdin", io.StringIO("".join(f"{v}\n" for v in inputs)))
