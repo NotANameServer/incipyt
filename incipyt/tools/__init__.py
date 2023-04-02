a8133afa1d8bca80d961e90e0996d5dd37ef7252
Florian Blanchet <florian.blanchet.supop@gmail.com>
2023-04-02 20:05:51 +0300

    fix(build): make legacy Setuptools tool compliant with PEP 517 specification

--- incipyt/tools/__init__.py
+++ incipyt/tools/__init__.py
@@ -3,5 +3,5 @@
 from incipyt.tools.base import Tool
 from incipyt.tools.git import Git
 from incipyt.tools.license import License
-from incipyt.tools.setuptools import Setuptools
+from incipyt.tools.pep517 import Setuptools
 from incipyt.tools.venv import Venv
--- incipyt/tools/pep517/__init__.py
+++ incipyt/tools/pep517/__init__.py
@@ -1,3 +1,4 @@
 """TO-DO."""
 
 from incipyt.tools.pep517.base import BuildSystem
+from incipyt.tools.pep517.setuptools import Setuptools
