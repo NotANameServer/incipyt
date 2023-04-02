ba8fe036431630f427b86ca53cd304805e9fa3f6
Florian Blanchet <florian.blanchet.supop@gmail.com>
2023-04-02 20:12:40 +0300

    feat(build): hatch support

--- incipyt/tools/pep517/__init__.py
+++ incipyt/tools/pep517/__init__.py
@@ -2,5 +2,6 @@
 
 from incipyt.tools.pep517.base import BuildSystem
 from incipyt.tools.pep517.flit import Flit
+from incipyt.tools.pep517.hatch import Hatch
 from incipyt.tools.pep517.pdm import PDM
 from incipyt.tools.pep517.setuptools import Setuptools
