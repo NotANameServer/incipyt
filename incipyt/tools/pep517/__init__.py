da5677a55d11af7b6286f8fba439423b9635a8c7
Florian Blanchet <florian.blanchet.supop@gmail.com>
2023-04-02 20:12:40 +0300

    feat(build): hatch support
    Behavior is compatible with PEPs since hatch v1.2.0.

--- incipyt/tools/pep517/__init__.py
+++ incipyt/tools/pep517/__init__.py
@@ -1,4 +1,5 @@
 from incipyt.tools.pep517.base import BuildSystem
 from incipyt.tools.pep517.flit import Flit
+from incipyt.tools.pep517.hatch import Hatch
 from incipyt.tools.pep517.pdm import PDM
 from incipyt.tools.pep517.setuptools import Setuptools
---
+++ incipyt/tools/pep517/hatch.py
@@ -0,0 +1,36 @@
+from incipyt import project, signals
+from incipyt._internal.dumpers import Toml
+from incipyt.tools import pep517
+
+
+class Hatch(pep517.BuildSystem):
+    """Scripts to add Hatch to :class:`incipyt.project._Structure`."""
+
+    def add_to_structure(self):
+        """Add hatch configuration to `project.structure`.
+
+        :file:`pyptoject.toml`
+
+        .. code-block::
+
+            [build-system]
+            build-backend = "hatchling.build"
+            requires = ["hatchling>=1.3.0"]
+
+        If this configuration cannot be populate like that, an error is raised.
+
+        :raises RuntimeError: If a build-system is already setup in pyproject.toml.
+        """
+        super().add_to_structure()
+
+        pyproject = project.structure.get_config_dict(Toml("pyproject.toml"))
+
+        if "build-system" in pyproject:
+            raise RuntimeError("Build system already registered.")
+
+        pyproject["build-system"] = {
+            "build-backend": "hatchling.build",
+            "requires": ["hatchling>=1.3.0"],
+        }
+
+        signals.build_dependency.emit(dep_name="hatch>=1.2.0")
