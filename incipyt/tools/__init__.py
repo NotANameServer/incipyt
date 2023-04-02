91c022a9f30e4aaac930ea3ee31c8d1aabe2915c
Florian Blanchet <florian.blanchet.supop@gmail.com>
2023-04-02 06:05:20 +0300

    feat(build): poetry supports

--- incipyt/tools/__init__.py
+++ incipyt/tools/__init__.py
@@ -4,4 +4,5 @@ from incipyt.tools.base import Tool
 from incipyt.tools.git import Git
 from incipyt.tools.license import License
 from incipyt.tools.pep517 import PDM, Flit, Setuptools
+from incipyt.tools.poetry import Poetry
 from incipyt.tools.venv import Venv
