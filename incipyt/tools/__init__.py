3eccc8d976af066285a6892ed2abb384544e8988
Florian Blanchet <florian.blanchet.supop@gmail.com>
2023-04-02 06:04:52 +0300

    feat(build): flit supports
    Behavior is compatible with PEPs since flit 3.4.0.

--- incipyt/tools/__init__.py
+++ incipyt/tools/__init__.py
@@ -1,5 +1,5 @@
 from incipyt.tools.base import Tool
 from incipyt.tools.git import Git
 from incipyt.tools.license import License
-from incipyt.tools.pep517 import Setuptools
+from incipyt.tools.pep517 import Flit, Setuptools
 from incipyt.tools.venv import Venv
--- incipyt/tools/pep517/__init__.py
+++ incipyt/tools/pep517/__init__.py
@@ -1,2 +1,3 @@
 from incipyt.tools.pep517.base import BuildSystem
+from incipyt.tools.pep517.flit import Flit
 from incipyt.tools.pep517.setuptools import Setuptools
