164b52215269abc11ada49aba8c3414f924e3583
Julien Castiaux <julien.castiaux@gmail.com>
2023-05-20 21:45:50 +0200

    fix(main): don't drop args[0] when importing

--- incipyt/__main__.py
+++ incipyt/__main__.py
@@ -155,11 +155,10 @@ def setup_logging(verbosity):
         warnings.filterwarnings("default")
 
 
-# Remove '' and current working directory from the first entry of sys.path, if
-# present to avoid using current directory in incipyt commands, when invoked as
-# python -m incipyt <command>
-if sys.path[0] in ("", os.getcwd()):  # noqa: PTH109
-    sys.path.pop(0)
-
 if __name__ == "__main__":
+    # Remove '' and current working directory from the first entry of
+    # sys.path, if present to avoid using current directory in incipyt
+    # commands, when invoked as python -m incipyt <command>
+    if sys.path[0] in ("", os.getcwd()):  # noqa: PTH109
+        sys.path.pop(0)
     sys.exit(main())
