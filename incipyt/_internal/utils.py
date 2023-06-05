f8e8a6a7e68f7a356a55b04b0202b3a3a91e6587
Julien Castiaux <julien.castiaux@gmail.com>
2023-06-05 20:06:15 +0200

    feat: use project.environ to store additional options

--- incipyt/_internal/utils.py
+++ incipyt/_internal/utils.py
@@ -52,6 +52,24 @@ def make_repr(obj, *args, **kwargs):
     return f"""{type(obj).__name__}({params})"""
 
 
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+
+    Source: https://github.com/python/cpython/blob/v3.11.3/Lib/distutils/util.py#L308-L321
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
+
+
 def unfold_dict(config):
     unfolded_config = {}
 
